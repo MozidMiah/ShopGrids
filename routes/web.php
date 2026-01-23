fcb08e7a12f65675ac6fa12f523917fdf46ecad8
MozidMiah <mazidkhan278@gmail.com>
2026-01-24 03:09:12 +0600

    admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\MyCommerceController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[MyCommerceController::class, 'index'])->name('home');
@@ -14,12 +15,7 @@ Route::get('/show-cart',[CartController::class, 'index'])->name('show-cart');
 Route::get('/checkout',[CheckoutController::class, 'index'])->name('checkout');
 
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])->group(function () {
+    
+    Route::get('/dashboard',[DashboardController::class, 'index'])->name('dashboard');
 });
